751fd59ecb7487587a3de3ada39cf9ecac11a523
jon <jonharder6@gmail.com>
2013-12-12 23:08:28 -0600

    Added support for default .do files and cleaned up

--- redo.hs
+++ redo.hs
@@ -1,20 +1,34 @@
+import Control.Monad (filterM, liftM)
+import Data.Maybe (listToMaybe)
 import System.Directory (renameFile, removeFile)
+import System.Directory (doesFileExist)
 import System.Environment (getArgs)
 import System.Exit (ExitCode(..))
+import System.FilePath (hasExtension, replaceBaseName, takeBaseName)
 import System.IO (hPutStrLn, stderr)
 import System.Process (createProcess, waitForProcess, shell)
 
 main :: IO ()
-main = do
-        args <- getArgs
-        mapM_ redo args
+main = mapM_ redo =<< getArgs
 
 redo :: String -> IO ()
-redo target = do
-        let tmp = target ++ "---redoing"
-        (_,_,_, ph) <- createProcess $ shell $ "sh " ++ target ++ ".do - - > " ++ tmp ++ " > " ++ tmp
-        exit <- waitForProcess ph
-        case exit of
-            ExitSuccess -> renameFile tmp target
-            ExitFailure code -> do hPutStrLn stderr $ "Redo script exited with non-zero exit code: " ++ show code
-                                   removeFile tmp
+redo target = maybe printMissing redo' =<< redoPath target
+ where redo' :: FilePath -> IO ()
+       redo' path = do
+         (_,_,_, processHandle) <- createProcess $ shell $ cmd path
+         exit <- waitForProcess processHandle
+         case exit of
+               ExitSuccess -> do
+                   renameFile tmp target
+                   putStrLn "Redo was successful"
+               ExitFailure code -> do hPutStrLn stderr $ "Redo script exited with non-zero exit code: " ++ show code
+                                      removeFile tmp
+       tmp = target ++ "---redoing"
+       printMissing = error $ "No .do file found for target '" ++ target ++ "'"
+       cmd path = unwords ["sh", path ,"0", takeBaseName target, tmp ,">", tmp]
+
+redoPath :: FilePath -> IO (Maybe FilePath)
+redoPath target = listToMaybe `liftM` filterM doesFileExist candidates
+    where candidates = [target ++ ".do"] ++ if hasExtension target 
+                                            then [replaceBaseName target "default" ++ ".do"] 
+                                            else []
